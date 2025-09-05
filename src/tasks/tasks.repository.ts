ff4f9fd4988ed9065526ee89bbb7b0fb9de0124f
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-09-05 20:04:25 +0700

    feat: add repositery method feature

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,38 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export interface Task {
+  id: number;
+  content: string;
+}
+
+export class TasksRepository {
+  private readonly filePath = 'tasks.json';
+
+  async findAll(): Promise<Task[]> {
+    const data = await readFile(this.filePath, 'utf-8');
+    const tasks: Task[] = JSON.parse(data) as Task[];
+
+    return tasks;
+  }
+
+  async findOne(id: number): Promise<Task | undefined> {
+    const data = await readFile(this.filePath, 'utf-8');
+    const tasks: Task[] = JSON.parse(data) as Task[];
+
+    return tasks.find((task) => task.id === id);
+  }
+
+  async create(task: string): Promise<void> {
+    const data = await readFile(this.filePath, 'utf-8');
+    const tasks: Task[] = JSON.parse(data) as Task[];
+
+    const newTask = {
+      id: tasks[tasks.length - 1].id + 1,
+      content: task,
+    };
+
+    tasks.push(newTask);
+
+    await writeFile(this.filePath, JSON.stringify(tasks, null, 2), 'utf-8');
+  }
+}
